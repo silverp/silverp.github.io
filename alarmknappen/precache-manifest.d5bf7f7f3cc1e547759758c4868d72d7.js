6b31de395e1223d367d553ab66e77553f500f6b7
Phuc Vo <phuc@Phuc-Macbook.local>
2019-10-22 18:45:51 +0700

    m

---
+++ alarmknappen/precache-manifest.d5bf7f7f3cc1e547759758c4868d72d7.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "550572777ac52006b4b2a5d46b065880",
+    "url": "/static/media/sidemenu.55057277.png"
+  },
+  {
+    "revision": "b250f79ebb570e759924",
+    "url": "/static/js/app.1df34e52.chunk.js"
+  },
+  {
+    "revision": "08461e7aeea309fd101527cdaf512201",
+    "url": "/static/js/app.1df34e52.chunk.js.gz"
+  },
+  {
+    "revision": "42f5a82c70ffc7caf9b00156da475d8e",
+    "url": "/static/media/appstore.42f5a82c.png"
+  },
+  {
+    "revision": "6165c9d7a2e729ba57b23dd93add5366",
+    "url": "/static/media/back-icon-mask.6165c9d7.png"
+  },
+  {
+    "revision": "207ce232890836d7fb106d0f46862fb9",
+    "url": "/static/media/phuc.207ce232.png"
+  },
+  {
+    "revision": "2d4472100ecdb616f18f39173819615e",
+    "url": "/static/media/login.2d447210.png"
+  },
+  {
+    "revision": "89f6c7e58f4d03f5c9957a237ca7488e",
+    "url": "/webpack.config.js.gz"
+  },
+  {
+    "revision": "64efebf6b93e84d5d541da88aa26da33",
+    "url": "/static/js/runtime~app.306130d3.js.gz"
+  },
+  {
+    "revision": "3c78beeb35099fc535f73fb86d3aa035",
+    "url": "/webpack.config.js"
+  },
+  {
+    "revision": "2e2ec819d7ff9c3805d7acf94bb614c2",
+    "url": "/static/media/splash_tablet.2e2ec819.png"
+  },
+  {
+    "revision": "bed2db377b010cb645a978a1def843dd",
+    "url": "/static/media/header.bed2db37.png"
+  },
+  {
+    "revision": "01748bfc9c51c385bd7e",
+    "url": "/static/js/runtime~app.306130d3.js"
+  },
+  {
+    "revision": "8915d8c48ebe6b10ca24e6b4b4c8fc43",
+    "url": "/./fonts/HelveticaNeue-Medium.ttf"
+  },
+  {
+    "revision": "c605a83cef1a5067421f7e913ca11895",
+    "url": "/static/js/2.a34c0038.chunk.js.gz"
+  },
+  {
+    "revision": "21f9008afd2161623e22631edc78204d",
+    "url": "/./fonts/HelveticaNeue-Thin.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "79f24f1f8f2cabd37ac5",
+    "url": "/static/js/2.a34c0038.chunk.js"
+  },
+  {
+    "revision": "3c596a4da75ab4390ba824aebec539a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f190fe43090be75845a450ca73b3f910",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "2fc665f3118f89e3eb1cbc9c1639a0bb",
+    "url": "/./fonts/Questrial-Regular.ttf"
+  },
+  {
+    "revision": "54a91b0619ccf9373d525109268219dc",
+    "url": "/./fonts/Roboto.ttf"
+  },
+  {
+    "revision": "99fc0816a09395454061301fefa42bf1",
+    "url": "/./fonts/Roboto_medium.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "9e43a811ad223c874391053a01f79d25",
+    "url": "/./fonts/HelveticaNeue-Light.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  }
+];
